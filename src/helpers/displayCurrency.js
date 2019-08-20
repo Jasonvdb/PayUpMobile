0022c94ec2ac13ee8a6c796f676c0197d739e558
Jason <jayvdb1@gmail.com>
2019-08-20 18:37:05 +0200

    Improved sats currency conversion

--- src/helpers/displayCurrency.js
+++ src/helpers/displayCurrency.js
@@ -1,10 +1,9 @@
 export default value => {
-	const sats = Math.round(value * 100000000).toString();
+	const sats = parseInt((value * 100000000).toFixed(0));
 
-	const numberString = `${sats.slice(0, -3)}`.replace(
-		/\B(?=(\d{3})+(?!\d))/g,
-		" ",
-	);
+	const numberString = `${sats}`
+		.slice(0, -3)
+		.replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 
 	return `${numberString}k sats`;
 };
